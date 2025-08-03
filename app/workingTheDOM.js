38b3a9a56f2fb487e9ce35e55a9451c58c9ee63b
jimbusCodes <jimbuscodes@gmail.com>
2025-08-03 18:34:59 -0400

    solved task 3

--- app/workingTheDOM.js
+++ app/workingTheDOM.js
@@ -4,3 +4,10 @@ document.getElementById("title").innerHTML = "DOM Manipulation";
 // task 2
 const containerDiv = document.getElementById("container");
 containerDiv.style.backgroundColor = "lightblue";
+
+// task 3
+const unorderedList = document.querySelector("ul");
+const newLi = document.createElement("li");
+
+newLi.innerHTML = "Item 7";
+unorderedList.append(newLi);
