50ec83d17a7013ba5d671e8585666421e411273a
jimbusCodes <jimbuscodes@gmail.com>
2025-08-03 18:29:15 -0400

    solved task 2

--- app/workingTheDOM.js
+++ app/workingTheDOM.js
@@ -1,2 +1,6 @@
 // task 1
 document.getElementById("title").innerHTML = "DOM Manipulation";
+
+// task 2
+const containerDiv = document.getElementById("container");
+containerDiv.style.backgroundColor = "lightblue";
